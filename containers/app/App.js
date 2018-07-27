0e21bd2116b59a43fd6f0a635c4f94933f50d182
Unknown <sbaudet@entreloups.com>
2018-07-27 16:41:12 +0200

    change default view

---
+++ containers/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import './App.styles';
+
+const App = () => (
+    <View>
+        <Text>Prout</Text>
+    </View>
+)
+
+export default App
