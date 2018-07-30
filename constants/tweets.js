f67ee12147b617d8003df9f8d5dddb90d4d67df2
Unknown <sbaudet@entreloups.com>
2018-07-30 11:53:02 +0200

    tweets creation

---
+++ constants/tweets.js
@@ -0,0 +1,21 @@
+const tweets = [{
+    _id: 0,
+    date: '22/05/2017',
+    username : 'Coralie',
+    tweet: 'Quelqu\'un pour une ranked ?',
+    avatar: 'assets/images.png',
+}, {
+    _id: 1,
+    date: '24/05/2017',
+    username : 'Coraliedu36',
+    tweet: 'Allo ?',
+    avatar: 'assets/images.png',
+}, {
+    _id: 2,
+    date: '26/05/2017',
+    username : 'Coraliedu45',
+    tweet: 'Pour info, je suis actuellement en live sur tweeche en train de faire une LAN avec ma mère',
+    avatar: 'assets/images.png',
+}]
+
+export default tweets
